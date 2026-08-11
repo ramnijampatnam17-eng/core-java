d836eea0e6d6bb1732c3f9e84abab3645ace041a
saini <sainijampatnam123@gmail.com>
2026-08-11 22:51:25 +0530

    TestDsa

---
+++ myfirstcorejavaproject/src/com/javaintroduction/TestDsa2.java
@@ -0,0 +1,19 @@
+package com.javaintroduction;
+
+public class TestDsa2 {
+
+	public static void main(String[] args) {
+		String[] names = new String[5];
+		names[0] = "sai";
+		names[1] = "ram";
+		names[2] = "shiva";
+		names[3] = "venkat";
+		names[4] = "gopi";
+		for(int i=0;i<names.length;i++) {
+			System.out.println(names[i]);
+		}
+
+
+	}
+
+}
