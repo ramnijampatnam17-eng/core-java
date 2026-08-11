1ebd4b31a007dde6e1fd1ae87306b8c9d5c5321f
saini <sainijampatnam123@gmail.com>
2026-08-11 22:33:57 +0530

    TestDsa

---
+++ myfirstcorejavaproject/src/com/javaintroduction/TestDsa1.java
@@ -0,0 +1,17 @@
+package com.javaintroduction;
+
+public class TestDsa1 {
+
+	public static void main(String[] args) {
+		int[] numbers;
+		numbers = new int[5];
+		numbers[0] = 15;
+		numbers[1] = 25;
+		numbers[2] = 35;
+		numbers[3] = 45;
+		numbers[4] = 55;
+		for(int i=0;i<numbers.length;i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+}
